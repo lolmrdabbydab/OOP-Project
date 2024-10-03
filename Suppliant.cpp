e14eab8f2dc99c11c0678be1052916d945c490fc
lolmrdabbydab <thaitimmy22@gmail.com>
2024-10-03 14:38:33 +0930

    Update

--- Suppliant.cpp
+++ Suppliant.cpp
@@ -11,6 +11,20 @@ using namespace std;
 // Constructors
 Suppliant::Suppliant(vector<double> costRefList, vector<double> costList) : costRefList(costRefList), costList(costList) {}
 //Suppliant::Suppliant() : Suppliant(vector<double>(), vector<double>()) {}
+    /*
+    -= Default Suppliant Inventory =-
+    Egg (1): Free Range Hens Egg
+    Egg (2): Cage Free Duck Egg
+    Milk (1): Cow Full Fat
+    Milk (1): Almond Vegan
+    Meat (1): Chicken Wing
+    Meat (2): Cow Sirloin
+    Toy (1): PlayDough Sand (1 year old)
+    Toy (2): Lego Figure (3 year old)
+    Soap (1): Dove Shampoo
+    Soap (2): Dove Body Wash
+    */
+
 
 // Getters
 vector<double> Suppliant::get_costRefList() { return costRefList; }
