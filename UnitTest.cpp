59161e3738634d0ded2cacda373beb85bfd63be7
lolmrdabbydab <thaitimmy22@gmail.com>
2024-10-15 18:45:06 +1030

    Update

--- UnitTest.cpp
+++ UnitTest.cpp
@@ -1,14 +1,32 @@
-// Implementation file for UnitTest class
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <limits>
+#include <ctime>
+#include <vector>
+#include <map>
+#include <iomanip>
+
+#include "Printable.h"
+#include "StoreBase.h"
+#include "Store.h"
+#include "Suppliant.h"
+#include "Item.h"
+#include "PerishableItem.h"
+#include "Egg.h"
+#include "Meat.h"
+#include "Milk.h"
+#include "Soap.h"
+#include "Toy.h"
 #include "UnitTest.h"
 
-UnitTest::UnitTest()
-{
-	// Constructor implementation
-}
+// Constructors
+UnitTest::UnitTest() {}
+
+// Methods
 
 
-UnitTest::~UnitTest()
-{
-	// Destructor implementation
-}
+// Destructor
+UnitTest::~UnitTest() {}
 
--- UnitTest.h
+++ UnitTest.h
@@ -1,13 +1,35 @@
-// Header file for UnitTest class
-#ifndef UnitTest_H
-#define UnitTest_H
+#ifndef UNITTEST_H
+#define UNITTEST_H
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <limits>
+#include <ctime>
+#include <vector>
+#include <map>
+#include <iomanip>
+
+#include "Printable.h"
+#include "StoreBase.h"
+#include "Store.h"
+#include "Suppliant.h"
+#include "Item.h"
+#include "PerishableItem.h"
+#include "Egg.h"
+#include "Meat.h"
+#include "Milk.h"
+#include "Soap.h"
+#include "Toy.h"
 
 class UnitTest
 {
+private:
+    
 public:
-	UnitTest();
-	~UnitTest();
+    UnitTest();
+    ~UnitTest();
 };
 
-#endif // UnitTest_H
+#endif
